3d79bb038eb5ed6a65230be5d152ef8a91f2726f
苏灿榕 <sucanrong@qipeipu.com>
2021-05-10 23:31:38 +0800

    add keymap for open and focus event

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,6 @@
+/**
+ * 限制插件只能在https/https协议的页面中激活
+ */
 chrome.runtime.onInstalled.addListener(() => {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -13,9 +16,24 @@ chrome.runtime.onInstalled.addListener(() => {
   })
 })
 
+/**
+ * 存入localStorage
+ */
 chrome.runtime.onMessage.addListener(async (req) => {
   const { action, data } = req
   chrome.storage.sync.set({ [action]: data }, () => {
     console.log('set successed!')
   })
+})
+
+/**
+ * 快捷键聚焦
+ */
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'trigger-focus') {
+    const views = chrome.extension.getViews({ type: 'popup' })
+    if (views.length > 0) {
+      views[0].document.getElementsByTagName('input')[0].focus()
+    }
+  }
 })
