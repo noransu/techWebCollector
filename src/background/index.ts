2937d7076a301ffe8d04cf337574c3f668a5b4c0
苏灿榕 <sucanrong@qipeipu.com>
2021-05-10 21:37:50 +0800

    open extension when page is not empty

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { schemes: ['https', 'http'] }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ])
+  })
+})
+
 chrome.runtime.onMessage.addListener(async (req) => {
   const { action, data } = req
   chrome.storage.sync.set({ [action]: data }, () => {
